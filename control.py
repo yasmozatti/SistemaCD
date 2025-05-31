1bc8369a6bdf0e0fe36e078d4e384efc8c6238c5
yasmozatti <yasmimmozatti@gmail.com>
2025-05-31 14:27:23 -0300

    Adicionando valores para a tabela

--- control.py
+++ control.py
@@ -57,6 +57,11 @@ def chama_segunda_tela():
     segunda_tela.tableWidget.setRowCount(len(dados_lidos))
     segunda_tela.tableWidget.setColumnCount(5)
 
+    for i in range(0, len(dados_lidos)):
+        for j in range(0,5):
+            segunda_tela.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
+            
+
 
 app = QtWidgets.QApplication([])
 formulario =uic.loadUi("form.ui")
